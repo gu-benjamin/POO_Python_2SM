dcc62632b5765b849f9836d7eecd751ac6d02504
Gustavo Benjamin <gustasbenjamin@gmail.com>
2024-08-20 22:17:09 -0300

    desafio extra 90%

--- Sistema.py
+++ Sistema.py
@@ -16,7 +16,7 @@ def main():
     opcao = input('Escolha uma opção:\n1 - Pessoa Física\n2 - Pessoa Jurídica\n0 - Sair\n\nInsira aqui: ')
     if opcao == '1':
       while True: 
-        opcao_pf = input('Escolha uma opção:\n1 - Cadastrar Pessoa Física\n2 - Listar Pessoa Física\n3 - Remover pessoa física\n0 - Voltar\n\nInsira aqui: ')
+        opcao_pf = input('Escolha uma opção:\n1 - Cadastrar Pessoa Física\n2 - Listar Pessoa Física\n3 - Remover pessoa física\n4 - Atualizar Pessoa Física\n0 - Voltar\n\nInsira aqui: ')
         # 1 - Cadastrar uma Pessoa Física
         if opcao_pf == '1':
           nova_pf = PessoaFisica()
@@ -74,6 +74,85 @@ def main():
                 pessoa_encontrada = True
                 print('Pessoa física removida com sucesso! Pressione qualquer tecla para voltar.')
                 break
+
+            if not pessoa_encontrada:
+              print('Pessoa não encontrada. Pressione qualquer tecla para voltar.\n')
+
+            input()
+          else:
+            print('Impossível realizar ação. Não há pessoas físicas registradas.\n')
+        
+        elif opcao_pf == '4':
+          if lista_pf:
+
+            cpf_atualizar = input('Digite o cpf da pessoa desejada para remove-la da lista: ').strip()
+            pessoa_encontrada = False
+
+            for pf in lista_pf:
+              if pf.cpf == cpf_atualizar:
+                pessoa_encontrada = True
+                print('Pessoa Encontra! Selecione a informação que deseja atualizar:\n')
+                print('1 - Nome\n2 - CPF\n3 - Data de nascimento\n4 - Rendimento\n5 - Logradouro\n6 - Número Logradouro\n7 - Endereco comercial\n0 - Cancelar Ação\n')
+
+                opcao_pf_alt = input('Insira sua opção: ')
+
+                if opcao_pf_alt == '1':
+                  pf.nome = input('Digite o nome da pessoa física: ')
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pf_alt == '2':
+                  pf.cpf = input('Digite o cpf da pessoa física: ').strip()
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pf_alt == '3':
+                  try:
+                    data_nascimento = datetime.strptime(input('Digite a data de nascimento da pessoa física (dd/MM/aaaa): ').strip(), '%d/%m/%Y').date()
+                    idade = (date.today() - data_nascimento).days // 365
+
+                    if idade >= 18:
+                      pf.data_nascimento = data_nascimento
+                      print('A pessoa tem mais de 18 anos. Continuando as alterações...')
+                    else:
+                      print('A pessoa tem menos de 18 anos. Impossivel continuar. Retornando...')
+                      break # Retorna ao menu de pessoa fisica
+
+                  except:
+                    print('Um erro ocorreu com a idade. Retornando ao menu...')
+                    break
+
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pf_alt == '4':
+                  pf.rendimento = float(input('Digite o rendimento da pessoa física (somente números): '))
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pf_alt == '5':
+                  pf.endereco.logradouro = input('Digite o logradouro: ')
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pf_alt == '6':
+                  pf.endereco.numero = input('Digite o número: ')
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pf_alt == '7':
+                  end_comercial = input('Este endereço é comercial? S/N: ').strip().upper()
+                  pf.endereco.endereco_comercial = end_comercial == 'S'
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pf_alt == '0':
+                  print('Operação cancelada. Pressione qualquer tecla para voltar.')
+                  break
+
+                else:
+                  print('Opção invalida. Pressione qualquer tecla para voltar.')
+                  break
 
             if not pessoa_encontrada:
               print('Pessoa não encontrada. Pressione qualquer tecla para voltar.\n')
@@ -146,6 +225,71 @@ def main():
             input()
           else:
             print('Impossível realizar ação. Não há pessoas jurídicas registradas.\n')
+        
+        elif opcao_pj == '4':
+          if lista_pj:
+
+            cnpj_atualizar = input('Digite o cnpj da pessoa desejada para remove-la da lista: ').strip()
+            pj_encontrada = False
+
+            for pj in lista_pj:
+              if pj.cpj == cnpj_atualizar:
+                pj_encontrada = True
+                print('Pessoa Encontra! Selecione a informação que deseja atualizar:\n')
+                print('1 - Nome\n2 - CNPJ\n3 - Data de nascimento\n4 - Rendimento\n5 - Logradouro\n6 - Número Logradouro\n7 - Endereco comercial\n0 - Cancelar Ação\n')
+
+                opcao_pj_alt = input('Insira sua opção: ')
+
+                if opcao_pj_alt == '1':
+                  pj.nome = input('Digite o nome da pessoa física: ')
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pj_alt == '2':
+                  pj.cpj = input('Digite o cnpj da pessoa física: ').strip()
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pj_alt == '3':
+
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pj_alt == '4':
+                  pj.rendimento = float(input('Digite o rendimento da pessoa física (somente números): '))
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pj_alt == '5':
+                  pj.endereco.logradouro = input('Digite o logradouro: ')
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pj_alt == '6':
+                  pj.endereco.numero = input('Digite o número: ')
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pj_alt == '7':
+                  end_comercial = input('Este endereço é comercial? S/N: ').strip().upper()
+                  pf.endereco.endereco_comercial = end_comercial == 'S'
+                  print('Pessoa física atualizada com sucesso! Pressione qualquer tecla para voltar.')
+                  break
+
+                elif opcao_pf_alt == '0':
+                  print('Operação cancelada. Pressione qualquer tecla para voltar.')
+                  break
+
+                else:
+                  print('Opção invalida. Pressione qualquer tecla para voltar.')
+                  break
+
+            if not pj_encontrada:
+              print('Pessoa não encontrada. Pressione qualquer tecla para voltar.\n')
+
+            input()
+          else:
+            print('Impossível realizar ação. Não há pessoas físicas registradas.\n')
         
         elif opcao_pj == '0':
           print('Voltando ao menu anterior')
